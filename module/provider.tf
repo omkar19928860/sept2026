60bee24cd86d47802e7a6db541ee30e5ff6bd8b9
omka1992 <127044732+omka1992@users.noreply.github.com>
2026-08-31 22:03:18 +0530

    code

---
+++ module/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+   backend "azurerm" {
+    resource_group_name  = "omkarrg"
+    storage_account_name = "omkarstoragea"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "6e65d60a-bc12-4f70-8dcd-6cdb4da48e3e"
+}
+
